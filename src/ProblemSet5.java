7997cbad85fb34ac8f77adfcc5492fe9b4766771
Alex Vasquez <caalevas@gmail.com>
2019-11-06 11:44:42 -0500

    Fixed Exercise 1: null

--- src/ProblemSet5.java
+++ src/ProblemSet5.java
@@ -20,7 +20,7 @@ public class ProblemSet5 {
 
     public static void main(String[] args) {
         ProblemSet5 ps = new ProblemSet5();
-        //System.out.print(ps.surroundMe("aise ", "prra"));
+        System.out.print(ps.surroundMe("nk \n me", "dame"));
         //System.out.print(ps.endsMeet("killlakill", 2));
         //System.out.print(ps.middleMan("qw"));
         //System.out.print(ps.isCentered("qucckzz", "cck"));
@@ -29,7 +29,7 @@ public class ProblemSet5 {
         //System.out.print(ps.addMe("2 plus 2 is..."));
         //System.out.print(ps.sequence("          aa"));
         //System.out.print(ps.intertwine("", "el!"));
-        System.out.print(ps.isPalindrome(" "));
+        //System.out.print(ps.isPalindrome(" "));
     }
 
     /*
@@ -40,7 +40,7 @@ public class ProblemSet5 {
      */
 
     public String surroundMe(String in, String out) {
-        if (in == null || out.length() != 4) {
+        if (in == null || out == null || out.length() != 4) {
             return in;
         }
         return out.substring(0, 2) + in + out.substring(out.length() - 2, out.length());
